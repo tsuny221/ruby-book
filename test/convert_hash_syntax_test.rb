71942647ceb3dd5f5fd0511c743c5f9afc99c77a
tsuny221 <kingoftsuny@gmail.com>
2020-05-29 22:17:31 +0900

    [Update] 6.3

---
+++ test/convert_hash_syntax_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require './lib/convert_hash_syntax'
+
+class ConvertHashSyntaxTest < MiniTest::Test
+	def test_convert_hash_syntax
+		old_syntax = <<~TEXT
+		{
+			:name => 'Alice',
+			:age=>20,
+			:gender => :female
+		}
+		TEXT
+		expected = <<~TEXT
+		{
+			name: 'Alice',
+			age: 20,
+			gender: :female
+		}
+		TEXT
+		assert_equal expected, convert_hash_syntax(old_syntax)
+		actual = convert_hash_syntax(old_syntax)
+	    puts actual
+		assert_equal expected, actual
+	end
+end
